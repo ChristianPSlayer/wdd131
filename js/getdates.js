197b9381fd66535f93ba9e4a1849868803159cf0
ChristianPSlayer <chiki0930@gmail.com>
2026-01-15 12:24:05 -0500

    getting better

--- js/getdates.js
+++
@@ -1,5 +0,0 @@
-
-const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
-
-document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
