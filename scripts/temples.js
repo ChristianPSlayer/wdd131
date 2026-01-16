7c02bd96eaecca86e064c21da0d8cc800cffe787
ChristianPSlayer <chiki0930@gmail.com>
2026-01-16 14:22:52 -0500

    week02

---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById('currentYear').textContent = currentYear;
+document.getElementById('lastModified').innerHTML = `last Modified: ${document.lastModified}`;
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+hamButton.addEventListener('click', ()=>{
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
