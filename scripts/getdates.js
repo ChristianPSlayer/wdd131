8d56cfc080a4ba55b0933d3ccd9fb74849b7f3ae
ChristianPSlayer <chiki0930@gmail.com>
2026-01-31 18:09:50 -0500

    countrypage

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,5 @@ const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
 document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+console.log("getdates.js file loaded successfully.");
