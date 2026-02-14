31f2a670a153dbc096ee944f43d6f06737c9a918
ChristianPSlayer <chiki0930@gmail.com>
2026-02-14 18:18:12 -0500

    Form

--- project/week05/scripts/form.js
+++ project/week05/scripts/form.js
@@ -1,9 +1,30 @@
 const products = [
-    { id: 1, name: 'Laptop', price: 999.99 },
-    { id: 2, name: 'Smartphone', price: 499.99 },
-    { id: 3, name: 'Tablet', price: 299.99 },
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
 ];
-
 document.addEventListener('DOMContentLoaded', () => {
     const productSelect = document.querySelector('#product');
 
